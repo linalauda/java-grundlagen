4fed508de98cbc5a7a8969a725a7afa1ad2806b0
Alina Kudrina <linakudrina@gmail.com>
2025-09-16 14:51:45 -0400

    lektion 16 Ueberladene Methoden

---
+++ tag-23-overloadedMethods/src/OverloadedMethods.java
@@ -0,0 +1,17 @@
+public class OverloadedMethods {
+    public static void main( String[] args ) {
+    System.out.println();
+    //print
+    System.out. print("World");
+    System.out.print (' ');
+    System.out.print(5678);
+//println
+    System.out.println(); // -> print + ln (line)
+        //printf
+        System.out.printf(" Hello %s%n" , "World");
+        int a = 12;
+        int b = 234;
+    System.out.printf("%s * %s = %s%n", a, b, a * b );
+    //                  a   b       a*b
+    }
+}
