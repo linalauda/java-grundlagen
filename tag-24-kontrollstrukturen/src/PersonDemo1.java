12b373defd6484ef879cf7bb8ffa6d6690687ceb
Alina Kudrina <linakudrina@gmail.com>
2025-09-22 19:28:29 -0400

    tag 24 anweisungen

---
+++ tag-24-kontrollstrukturen/src/PersonDemo1.java
@@ -0,0 +1,18 @@
+public class PersonDemo1 {
+    public static void main(String[] args) {
+        // Ausdrucksanweisungen
+        String name = "Anna";      // Zuweisung einer Variable
+        int alter = 25;            // Zuweisung einer Zahl
+        double groesse = 1.70;     // Zuweisung einer Kommazahl
+
+        alter++;                   // Inkrement (Ausdrucksanweisung)
+
+        System.out.println("Name: " + name);     // Methodenaufruf (auch Ausdrucksanweisung)
+        System.out.println("Alter: " + alter);   // Ausgabe
+        System.out.println("Größe: " + groesse + " m");
+
+        ; // leere Anweisung (macht nichts, wird aber akzeptiert)
+    }
+}
+
+
