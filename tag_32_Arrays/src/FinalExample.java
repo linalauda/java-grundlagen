893906fd2ac9de313fc1c9c1e357c67528bc307e
Alina Kudrina <linakudrina@gmail.com>
2025-11-10 14:21:55 -0500

    FINAL Konstanten Variablen Methoden Classen

---
+++ tag_32_Arrays/src/FinalExample.java
@@ -0,0 +1,25 @@
+public class FinalExample {
+    public static void main(String[] args) {
+        final int MAX_SPEED = 120; //Konstante
+        System.out.println("Maximale Geschwindigkeit: " + MAX_SPEED);
+
+        //MAX_SPEED = 130;
+
+    class Fahrzeug {
+        public final void starten(){
+            System.out.println("Fahrzeug startet... ");
+        }
+    }
+    class Auto extends Fahrzeug {
+
+
+    }
+
+    final class Mathe {
+        public static double PI = 3.14159;
+
+    }
+    class MeineMathe extends Mathe
+
+    }
+}
